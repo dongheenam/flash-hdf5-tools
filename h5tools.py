1521c3e7780d4a0161589b116bd9a3347c58f149
Donghee Nam <donghee.nam@anu.edu.au>
2019-10-03 23:42:55 +1000

    power spectrum

--- h5tools.py
+++ h5tools.py
@@ -1,260 +1,106 @@
-"""
-MODULE h5tools
-
-DESCRIPTION
-    contains a list of helper functions that utilises h5py
-    to read FLASH hdf5 output files
-
-AUTHOR
-    Donghee NAM, 2019
-===============================================================================
-"""
 # DEPENDENCIES
 import h5py
 import numpy as np
-
+import pytools
+from scipy import fftpack
 
 """
-FUNCTION dict = h5tools.read_integer_parameters, read_integer_scalars,
-                        read_real_parameters, read_real_scalars,
+FUNCTION dict = read_parameters(h5f, dataname)
 
 DESCRIPTION
-    reads the parameters/scalars from a FLASH file and returns them as a dictionary
+    return the specified parameter group as a dictionary from the FLASH hdf5 file
 
 INPUTS
-    file (string)
-        the name of the hdf5 file
-    file (h5py.File)
-        the h5py file object of the hdf5 file
-(the type should be given with the isH5pyObject parameter below;
-it is assumed to be a h5py object by default)
-
-PARAMETERS
-    isFilename (boolean) = False
-        if false, file is assumed to be a h5py object
-        if true, file is assumed to be the filename for the FLASH file
-    verbose (boolean) = False
-        if True then the program will print the information
+    h5f (h5py.File)
+        the h5py File object containing the hdf5 file
+    dataname (string)
+        the keyword for the parameter group
+          e.g.: ['integer runtime parameters', 'real runtime parameters',
+                 'integer scalars', 'real scalars']
 
 OUTPUTS
     dict (dictionary)
-        the dictionary containing the parameters/scalars
-        the values can be accessed via their names e.g. dict['nxb']
+        contains the parameters, which can be accessed with their names e.g. dict['nxb']
 """
-def read_integer_parameters(file, isFilename=False, verbose=False) :
+def read_parameters(h5f, dataname) :
 
-  if isFilename :
-    if verbose : print("opening the hdf5 file...")
-    h5f = h5py.File(file,'r')
-  else :
-    h5f = file
-
-  # the parameters
-  params = dict(h5f['integer runtime parameters'])
+  # parameters
+  params = dict(h5f[dataname])
 
   # strip the spaces for easier access
   keys_stripped = [keys.strip() for keys in params.keys()]
   for newkey, oldkey in zip(keys_stripped, params.keys()) :
     params[newkey] = params.pop(oldkey)
 
-  if verbose : print("integer parameters read")
-  return params
-
-def read_integer_scalars(file, isFilename=False, verbose=False) :
-
-  if isFilename :
-    if verbose : print("opening the hdf5 file...")
-    h5f = h5py.File(file,'r')
-  else :
-    h5f = file
-
-  # the parameters
-  params = dict(h5f['integer scalars'])
-
-  # strip the spaces for easier access
-  keys_stripped = [keys.strip() for keys in params.keys()]
-  for newkey, oldkey in zip(keys_stripped, params.keys()) :
-    params[newkey] = params.pop(oldkey)
-
-  if verbose : print("integer scalars read")
-  return params
-
-def read_real_parameters(file, isFilename=False, verbose=False) :
-
-  if isFilename :
-    if verbose : print("opening the hdf5 file...")
-    h5f = h5py.File(file,'r')
-  else :
-    h5f = file
-
-  # the parameters
-  params = dict(h5f['real runtime parameters'])
-
-  # strip the spaces for easier access
-  keys_stripped = [keys.strip() for keys in params.keys()]
-  for newkey, oldkey in zip(keys_stripped, params.keys()) :
-    params[newkey] = params.pop(oldkey)
-
-  if verbose : print("real parameters read")
-  return params
-
-def read_real_scalars(file, isFilename=False, verbose=False) :
-
-  if isFilename :
-    if verbose : print("opening the hdf5 file...")
-    h5f = h5py.File(file,'r')
-  else :
-    h5f = file
-
-  # the parameters
-  params = dict(h5f['real scalars'])
-
-  # strip the spaces for easier access
-  keys_stripped = [keys.strip() for keys in params.keys()]
-  for newkey, oldkey in zip(keys_stripped, params.keys()) :
-    params[newkey] = params.pop(oldkey)
-
-  if verbose : print("real scalars read")
   return params
 
 """
-FUNCTION block_sizes = h5tools.read_block_sizes(file, isFilename=False, verbose=False)
+FUNCTION array = read_array(h5f, dataname)
 
 DESCRIPTION
-    reads the box sizes from a FLASH file and returns them as an array
+    return the specified array that does not need to be sorted
+    as a NumPy from the FLASH hdf5 file
 
 INPUTS
-    file (string or h5py.File)
-        see above
-
-PARAMETERS
-    isFilename=False, verbose=False
-        see above
+    h5f (h5py.File)
+        the h5py File object containing the hdf5 file
+    dataname (string)
+        the keyword for the array
+            e.g. ['block size', 'coordinates']
 
 OUTPUTS
-    block_sizes (float[:,3])
-        the NumPy array containing the box coordinates
+    array (float[: , ... , :])
+        the NumPy array containing the data
 """
-def read_block_sizes(file, isFilename=False, verbose=False) :
-
-  if isFilename :
-    if verbose : print("opening the hdf5 file...")
-    h5f = h5py.File(file,'r')
-  else :
-    h5f = file
+def read_array(h5f, dataname):
 
   # the parameters
-  block_sizes = np.array(h5f['block size'])
+  array = np.array(h5f[dataname])
 
-  if verbose : print("block sizes read")
-  return block_sizes
+  return array
 
 """
-FUNCTION box_coords = h5tools.read_box_coordinates(file, isFilename=False, verbose=False)
+FUNCTION data_sorted = sort(H5file, data)
 
 DESCRIPTION
-    reads the box coordinates from a FLASH file and returns them as an array
+    by default the FLASH file prints the field in [nProcs, nzb, nyb, nxb]
+    this method is returns the field in [N_x, N_y, N_z] dimensions
+    using the parameters stored in the H5file instance
 
 INPUTS
-    file (string or h5py.File)
-        see above
-
-PARAMETERS
-    isFilename=False, verbose=False
-        see above
+    H5file
+        the H5file instance
+    data (float[nProcs, nzb, nyb, nxb])
+        the raw data from the FLASH hdf5 file
 
 OUTPUTS
-    box_coords (float[:,3])
-        the NumPy array containing the box coordinates
+    data_sorted (float[N_x, N_y, N_z])
+        the 3D NumPy array containing the sorted data
 """
-def read_box_coordinates(file, isFilename=False, verbose=False) :
+def sort(H5file, data) :
 
-  if isFilename :
-    if verbose : print("opening the hdf5 file...")
-    h5f = h5py.File(file,'r')
-  else :
-    h5f = file
+  # some parameters required for the sorting
+  # they are read from the parent class (H5file)
 
-  # the parameters
-  box_coords = np.array(h5f['coordinates'])
-
-  if verbose : print("box coordinates read")
-  return box_coords
-
-"""
-FUNCTION field = h5tools.read_flash(file, field_name, isFilename=False, verbose=False)
-
-DESCRIPTION
-    reads a HDF5 file and return the specified 3D field
-
-INPUTS
-    file (string)
-        the name of the hdf5 file
-    file (h5py.File)
-        the h5py file object of the hdf5 file
-(the type should be given with the isH5pyObject parameter below;
-it is assumed to be a h5py object by default)
-    field_name (string)
-        the name of the field as specified in the hdf5 file
-
-PARAMETERS
-    isFilename (boolean) = False
-        if false, file is assumed to be a h5py object
-        if true, file is assumed to be the filename for the FLASH file
-    verbose (boolean) = False
-        if True then the program will print the information
-
-OUTPUTS
-    field (float[:, :, :])
-        the 3D numpy array containing the field information
-        the dimension should equal nxb*iprocs, nyb*jprocs, nzb*kprocs
-"""
-def read_flash(file, field_name, isFilename=False, verbose=False) :
-
-  if isFilename :
-    if verbose : print("opening the hdf5 file...")
-    h5f = h5py.File(file,'r')
-  else :
-    h5f = file
-
-  if verbose : print("reading {} from the file...".format(field_name))
-
-  # read the key parameters
-  int_param = read_integer_scalars(h5f)
-  nxb = int_param['nxb']
-  nyb = int_param['nyb']
-  nzb = int_param['nzb']
-  iprocs = int_param['iprocs']
-  jprocs = int_param['jprocs']
-  kprocs = int_param['kprocs']
-  dims = np.array([nxb*iprocs, nyb*jprocs, nzb*kprocs])
-
-  real_param = read_real_parameters(h5f)
-  xmin = real_param['xmin']
-  xmax = real_param['xmax']
-  ymin = real_param['ymin']
-  ymax = real_param['ymax']
-  zmin = real_param['zmin']
-  zmax = real_param['zmax']
-  L_bl = np.array([xmin, ymin, zmin])
-  L_tr = np.array([xmax, ymax, zmax])
+  dims = np.array( H5file.params['dims'] )
+  L_bl = np.array( H5file.params['Lmin'] )
+  L_tr = np.array( H5file.params['Lmax'] )
   L = L_tr - L_bl
   L_cell = L / dims
 
-  block_sizes = read_block_sizes(h5f)
-  box_coords = read_box_coordinates(h5f)
+  block_sizes = H5file.block_sizes
+  box_coords = H5file.block_coords
 
-  # the raw field array
-  # it has structure of [nProcs, nzb, nyb, nxb]
+  # the raw field array has structure of [nProcs, nzb, nyb, nxb]
   # so transposing is necessary to have shape of [nProcs, nxb, nyb, nzb]
-  if verbose : print("loading {} ...".format(field_name))
-  field_raw = np.transpose(np.array(h5f[field_name]), (0,3,2,1))
-  if verbose : print("{} loaded!".format(field_name))
+  data = np.transpose(data, (0,3,2,1))
 
   # place the boxes in the 3D array
-  field_sorted = np.zeros(dims)
+  data_sorted = np.zeros(dims)
 
-  if verbose : print("sorting commenced ...")
+  iprocs = H5file.params['iprocs']
+  jprocs = H5file.params['jprocs']
+  kprocs = H5file.params['kprocs']
   for proc_id in range(iprocs*jprocs*kprocs) :
 
     # bottom left corner
@@ -265,12 +111,252 @@ def read_flash(file, field_name, isFilename=False, verbose=False) :
     box_loc_tr = box_coords[proc_id] + block_sizes[proc_id]/2
     index_tr = np.rint( (box_loc_tr - L_bl)/L_cell ).astype(int)
 
-    if verbose : print("proc_id: {}  indices from {} to {}".format(proc_id, index_bl, index_tr))
-
     # insert the block
-    field_sorted[index_bl[0]:index_tr[0], index_bl[1]:index_tr[1], index_bl[2]:index_tr[2]] \
-                = field_raw[proc_id]
+    data_sorted[index_bl[0]:index_tr[0], index_bl[1]:index_tr[1], index_bl[2]:index_tr[2]] \
+                = data[proc_id]
 
-  if verbose : print("sorting completed!")
+  # update the sorted data
+  return data_sorted
 
-  return field_sorted
+"""
+================================================================================
+CLASS H5file(filename)
+
+DESCRIPTION
+    container for a FLASH hdf5 file and its parameters
+
+INPUTS
+    filename (string)
+        the name of the hdf5 file
+
+VARIABLES
+    H5file.filename (string)
+        the specified filename of the hdf5 file
+    H5file.h5f (h5py.File)
+        the h5py File object
+    H5file.params (dictionary)
+        the integer/real parameters/scalars from the file
+
+METHODS
+    H5file.new_dataset(dataname)
+        returns a scalar or vector dataset instance, depending on the data name
+================================================================================
+"""
+class H5file :
+
+  def __init__(self, filename) :
+    self.filename = filename
+    self.h5f = h5py.File(filename, 'r')
+
+    int_params = read_parameters(self.h5f, 'integer runtime parameters')
+    real_params = read_parameters(self.h5f, 'real runtime parameters')
+    int_scalars = read_parameters(self.h5f, 'integer scalars')
+    real_scalars = read_parameters(self.h5f, 'real scalars')
+    self.params = pytools.merge_dicts(int_params, real_params, int_scalars, real_scalars)
+
+    nxb = self.params['nxb']
+    nyb = self.params['nyb']
+    nzb = self.params['nzb']
+    iprocs = self.params['iprocs']
+    jprocs = self.params['jprocs']
+    kprocs = self.params['kprocs']
+    self.params['dims'] = [nxb*iprocs, nyb*jprocs, nzb*kprocs]
+
+    xmin = self.params['xmin']
+    xmax = self.params['xmax']
+    ymin = self.params['ymin']
+    ymax = self.params['ymax']
+    zmin = self.params['zmin']
+    zmax = self.params['zmax']
+    self.params['Lmin'] = [xmin, ymin, zmin]
+    self.params['Lmax'] = [xmax, ymax, zmax]
+    self.params['L'] = [xmax-xmin, ymax-ymin, zmax-zmin]
+
+    self.block_sizes = read_array(self.h5f, 'block size')
+    self.block_coords = read_array(self.h5f, 'coordinates')
+
+    print("{} read!".format(filename))
+    print("simulation time: {}".format(self.params['time']))
+    print("size           : {}".format(self.params['dims']))
+    print("domain         : {}".format(self.params['L']))
+
+  def new_dataset(self, dataname, save_mem=False) :
+    if dataname == 'dens' :  # for density field              => create a density data set
+      print("attempting to create a density dataset...")
+      return self.DensityDataset(self)
+    else :
+      try :         # see if the dataname exists              => create a scalar data set
+        print("attempting to create a scalar dataset: {}...".format(dataname))
+        return self.ScalarDataset(self, dataname)
+      except :
+        try :       # see if the dataname+"x","y","z" exists  => create a vector data set
+          print("attempting to create a vector dataset: {}...".format(dataname))
+          return self.VectorDataset(self, dataname, save_mem)
+        except :    # if the dataname does not exist within the hdf5 file => do nothing
+          print("dataname not recognised")
+
+  """
+  ================================================================================
+  CLASS H5file.ScalarDataset(object)
+
+  DESCRIPTION
+      container for a scalar field
+
+  INPUTS
+      H5file
+          the outer class H5file
+
+  VARIABLES
+      Dataset.H5file
+          the outer class
+      Dataset.data (np.array[N_x,N_y,N_z])
+          the raw field values
+
+  METHODS
+      mean = Dataset.mean()
+          returns the mean value of the data
+  ================================================================================
+  """
+  class ScalarDataset(object) :
+
+    def __init__(self, H5file, dataname) :
+      self.H5file = H5file
+      self.data = sort(H5file, H5file.h5f[dataname])
+
+    def mean(self) :
+      return np.mean(self.data)
+
+    def calc_ft(self) :
+      print("performing fast fourier transform...")
+      rms_field = np.sqrt(np.average(self.data**2))
+
+      self.ft = np.fft.fftshift(np.fft.fftn(self.data)) / np.product(self.H5file.params['dims'])
+      print("... fft completed!")
+
+      sum_power = np.sum(np.abs(self.ft)**2)
+      print("sum_power        : {}".format(sum_power))
+      print("rms_squared_field: {}".format(rms_field**2))
+
+    def calc_ps(self) :
+      print("calculating the power spectrum...")
+      try :
+        self.ft
+        print("fourier field found!")
+      except AttributeError :
+        print("fourier field not detected!")
+        self.calc_ft()
+
+      ft_squared = np.abs(self.ft)**2
+      sum_power = np.sum(ft_squared)
+
+      dims = np.array(self.H5file.params['dims'])
+      nx, ny, nz = dims
+      L = np.array(self.H5file.params['L'])
+      # [-N, -(N-1), ..., -1, 0, 1, ..., N-2, N-1]
+      kx_list = ( np.arange(-nx//2,nx//2,1) )/L[0]
+      ky_list = ( np.arange(-ny//2,ny//2,1) )/L[1]
+      kz_list = ( np.arange(-nz//2,nz//2,1) )/L[2]
+
+      # tells the k of the corresponding element of 'power_field'
+      k_x, k_y, k_z = np.meshgrid(kx_list, ky_list, kz_list, indexing="ij")
+      k_3Darray = np.sqrt(k_x**2 + k_y**2 + k_z**2)
+
+      # physical limits to the wavenumbers
+      kmin = np.min(1.0/L)
+      kmax = np.min(0.5*dims/L)
+
+      # bins of wavenumbers
+      # first bin = (0.5 to 1.5), second bin = (1.5 to 2.5), ...
+      k_bins = np.arange(kmin, kmax, kmin) - 0.5*kmin
+
+      # returns the index of k_bins which each k_3Darray fits inside
+      bin_index = np.digitize(k_3Darray.flat, k_bins)
+
+      power_spectrum = np.zeros(len(k_bins))
+
+      for i in range(1,len(power_spectrum)) :
+          power_spectrum[i-1] = np.sum(ft_squared.flat[bin_index==i])
+          if i%10 == 0 :
+              print("power spectrum summed until k="+str(i))
+      print("completed!")
+
+      print("sum_powerspec    : {}".format(np.sum(power_spectrum)))
+
+      self.k = k_bins
+      self.ps = power_spectrum
+
+  """
+  ================================================================================
+  CLASS H5file.DensityDataset(H5file.ScalarDataset)
+
+  DESCRIPTION
+      container for the density field
+
+  INPUTS
+      H5file
+          the outer class H5file
+
+  VARIABLES
+      Dataset.H5file
+          the outer class
+      Dataset.data (np.array[N_x,N_y,N_z])
+          the raw field values
+
+  METHODS
+
+  ================================================================================
+  """
+  class DensityDataset(ScalarDataset) :
+
+    def __init__(self, H5file) :
+      super(H5file.DensityDataset,self).__init__(H5file, 'dens')
+
+    def set_log(self) :
+      self.data = np.log(self.data/self.mean())
+
+    def set_delta(self) :
+      self.set_log()
+      self.data = self.data - self.mean()
+
+  """
+  ================================================================================
+  CLASS H5file.VectorDataset(H5file, dataname)
+
+  DESCRIPTION
+      container for a vector field of data
+
+  INPUTS
+      H5file
+          the outer class H5file
+      dataname (string)
+          the keyword of the dataset within the file without x,y,z suffixes
+              e.g. ['vel', 'mag']
+
+      save_mem = False
+          if working on large data, it is often impossible
+          to work with all three field components
+          set True when you do not want to load all field components at once
+
+  VARIABLES
+      Dataset.H5file
+          the outer class
+      Dataset.datax (np.array[N_x,N_y,N_z])
+          the raw values of the x-component of the field
+      Dataset.datay (np.array[N_x,N_y,N_z])
+          the raw values of the y-component of the field
+      Dataset.dataz (np.array[N_x,N_y,N_z])
+          the raw values of the z-component of the field
+  ================================================================================
+  """
+  class VectorDataset(object) :
+
+    def __init__(self, H5file, dataname, save_mem=False) :
+      self.H5file = H5file
+      if save_mem :
+        self.datax = None
+        self.datay = None
+        self.dataz = None
+      else :
+        self.datax = sort(H5file, H5file.h5f[dataname+'x'])
+        self.datay = sort(H5file, H5file.h5f[dataname+'y'])
+        self.dataz = sort(H5file, H5file.h5f[dataname+'z'])
