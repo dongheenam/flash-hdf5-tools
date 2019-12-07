c1cbba56150bf7897e8e0211792e6ce1c1df16d4
Donghee Nam <dn8909@gadi-login-03.gadi.nci.org.au>
2019-12-08 09:19:23 +1100

    added quiet option

--- constants.py
+++ constants.py
@@ -13,7 +13,7 @@ class CST(object) :
     one may calculate from the parameters passed on as a dictionary
     or a h5tools.H5File object
     """
-    def __init__(self, H5File=None, filename=None, **params_custom) :
+    def __init__(self, H5File=None, filename=None, verbose=True, **params_custom) :
         # initial variables
         M_SOL = 1.98847542e33 # solar mass
         M_TOT = 387.5 * M_SOL # total mass of the system
@@ -27,38 +27,45 @@ class CST(object) :
         # read the custom parameters
         if 'L' in params_custom :
             L = params_custom['L']
-            print(f"L updated from kwargs to be: {L}")
+            if verbose :
+                print(f"L updated from kwargs to be: {L}")
         if 'C_S' in params_custom :
             C_S = params_custom['C_S']
-            print(f"C_S updated from kwargs to be: {C_S}")
+            if verbose :
+                print(f"C_S updated from kwargs to be: {C_S}")
         if 'MACH' in params_custom :
             MACH = params_custom['MACH']
-            print(f"MACH updated from kwargs to be: {MACH}")
+            if verbose :
+                print(f"MACH updated from kwargs to be: {MACH}")
 
         if 'RHO' in params_custom :
             RHO = params_custom['RHO']
-            print(f"RHO updated from kwargs to be: {RHO}")
+            if verbose :
+                print(f"RHO updated from kwargs to be: {RHO}")
             M_TOT = RHO * L**3
         elif 'M_TOT' in params_custom :
             M_TOT = params_custom['M_TOT']
-            print(f"M_TOT updated from kwargs to be: {M_TOT}")
+            if verbose :
+                print(f"M_TOT updated from kwargs to be: {M_TOT}")
             RHO = M_TOT / L**3
 
         # create a H5File instance if a filename is given
         if filename is not None :
             try :
-                H5File = h5tools.H5File(filename)
+                H5File = h5tools.H5File(filename, verbose=verbose)
             except KeyError :
-                H5File = h5tools.PartFile(filename)
+                H5File = h5tools.PartFile(filename, verbose=verbose)
 
         # read from the H5File instance if it exists
         if H5File is not None :
-            print("using the H5File instance for the constants...")
+            if verbose :
+                print("using the H5File instance for the constants...")
             L = H5File.params['L'][0]
             RHO = H5File.params['rho_ambient']
             M_TOT = L**3 * RHO
-            print(f"M_TOT updated from H5File to be: {M_TOT}")
-            print(f"L updated from H5File to be: {L}")
+            if verbose :
+                print(f"M_TOT updated from H5File to be: {M_TOT}")
+                print(f"L updated from H5File to be: {L}")
 
         # store the variables
         self.M_SOL = M_SOL
@@ -71,13 +78,21 @@ class CST(object) :
         self.SIGMA_V = C_S * MACH
         self.T_TURB = L / (2*self.SIGMA_V) # turbulent crossing time
         self.T_FF = np.sqrt(3*np.pi/(32*G*self.RHO)) # free-fall time
-        self.VIR = (5*self.SIGMA_V**2*L/2)/(3*G*M_TOT)
+        self.VIR = (5*self.SIGMA_V**2*L/2)/(3*G*M_TOT) # virial parameter
+
+        # if particle file is read, then calculate the SFE
+        if isinstance(H5File, h5tools.PartFile) :
+            if H5File.particles is None :
+                self.SFE = 0.0
+            else :
+                self.SFE = np.sum(H5File.particles['mass']) / M_TOT
 
     def show(self) :
         print(f"Turbulent crossing time: {self.T_TURB:.4E}")
         print(f"free-fall time         : {self.T_FF:.4E}")
         print(f"mean density           : {self.RHO:.4E}")
         print(f"virial parameter       : {self.VIR:.4f}")
+        print(f"SFE                    : {100.0*self.SFE:.2f}%")
 
 if __name__ == "__main__" :
     parser = argparse.ArgumentParser(description='')
