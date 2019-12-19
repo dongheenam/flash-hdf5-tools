a4890e622410e614f98037d79a07c12b1c2d2d17
Donghee Nam <dn8909@gadi-login-03.gadi.nci.org.au>
2019-12-20 10:54:18 +1100

    added sound speed

--- constants.py
+++ constants.py
@@ -1,7 +1,11 @@
 
-# physical constants
+# unit conversion (in cgs)
 M_SOL = 1.98847542e33   # solar mass
 AU = 1.495979e13        # astronomical unit
 LY = 9.460730e17        # light year
 PC = 3.085678e18        # parsec
 PC_flash = 3.086e18     # parsec as defined in flash.par
+
+# physical constants
+G = 6.67408e-8        # gravitational constant
+C_S = 0.2e5           # isothermal sound speed
