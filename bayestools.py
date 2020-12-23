244916777020ee9c878d1f99305eee152a742978
Donghee Nam <dn8909@gadi-login-06.gadi.nci.org.au>
2020-12-24 08:23:57 +1100

    added different prior for the parameters

--- bayestools.py
+++ bayestools.py
@@ -16,14 +16,14 @@ from scipy.stats import binom
 """
 CONSTANTS
 """
-M_MIN = 5e-7
-M_MAX = 2e-2 # 2e-2
-M_T_UPPER = 1.5e-3
+M_MIN = 0
+M_MAX = 0.1
+M_T_UPPER = 0.1
 
 # MCMC parameters
 N_WALKERS = 96
-N_BURNIN = 100
-N_CHAIN = 500
+N_BURNIN = 200
+N_CHAIN = 200
 
 """ THE CHABRIER IMF """
 # the lognormal part
@@ -51,33 +51,31 @@ def gen_imf_chab(theta, x) :
     # first find the coefficient to make the imf continuous
     cont = root_scalar(lambda x: imf_ln(theta, m_t) - x*imf_pl(theta, m_t), x0=1.0, x1=0.9)
     pl_factor = cont.root
-    #pl_factor = 0.4 * np.exp(-9.43e-2 * ( (np.log(m_t)-np.log(mu))/sigma )**2 ) * m_t**(-gamma) / sigma
 
     # then normalise the imf
     norm_factor1, _ = quad(lambda x: imf_ln(theta, x), M_MIN, m_t)
     norm_factor2, _ = quad(lambda x: pl_factor*imf_pl(theta, x), m_t, M_MAX)
-    #norm_factor1  = -2.88586*sigma*erf( 0.307*(np.log(M_MIN)-np.log(mu)) )
-    #norm_factor1 +=  2.88586*sigma*erf( 0.307*(np.log(m_t)-np.log(mu)) )
-    #norm_factor1 /= np.sqrt(2*np.pi) * sigma
-    #norm_factor2 = pl_factor/gamma * (M_MAX**gamma - m_t**gamma)
     try :
-        A = float(1/(norm_factor1 + norm_factor2))
+        A = float(1.0/(norm_factor1 + norm_factor2))
     except ZeroDivisionError :
         sys.exit(f"{norm_factor1}, {norm_factor2}")
 
     # return IMF(x)
-    return np.array([imf_chab(theta, x_elem, pl_factor=pl_factor, A=A) for x_elem in x])
+    try :
+        return np.array([imf_chab(theta, x_elem, pl_factor=pl_factor, A=A) for x_elem in x])
+    except TypeError :
+        return imf_chab(theta, x, pl_factor=pl_factor, A=A)
 
 """ model and probility distributions """
 # define the prior
 def ln_prior(theta) :
     sigma, mu, m_t, gamma = theta
 
-    in_range = (sigma>0 and M_MIN<mu<m_t and M_MIN<m_t<M_MAX and -4<gamma<0)
+    in_range = (sigma>0 and M_MIN<mu<m_t and M_MIN<m_t<M_T_UPPER and -4<gamma<0)
     if in_range :
         # prior goes here
-        #return (1+b**2)**(-1.5)
-        return 0
+        #return -3/2 * np.log(1+gamma**2)
+        return -3/2 * np.log(1+gamma**2) -0.5 * ( (m_t - 2.840e-3) / 2.586e-4 )**2
     else :
         # out of range
         return -np.inf
@@ -109,6 +107,7 @@ def fit_imf(masses_obs) :
 
     """ initialisation """
     print("initialising emcee...")
+    print(f"prior on M_T: {M_T_UPPER:.1E}")
     # initial guess
     theta_names = ['sigma', 'mu', 'm_t', 'gamma']
     theta_guess = [0.5, 5e-4, 1e-3, -1.35]
@@ -132,18 +131,36 @@ def fit_imf(masses_obs) :
         # the production run
         print("running productions...")
         sampler.reset()
-        pos_final, _, _ = sampler.run_mcmc(pos, N_CHAIN, progress=True)
+        sampler.run_mcmc(pos, N_CHAIN, progress=True)
+        flat_samples = sampler.get_chain(flat=True)
 
-    # print out the result
     print("fininshed!")
-    results = np.zeros((n_theta,3))
-    for i in range(n_theta):
-        quarts = np.percentile(pos_final[:, i], [16, 50, 84])
-        ranges = np.diff(quarts)
-        results[i] = (quarts[1], ranges[1], ranges[0])
-        print(f"{theta_names[i]:10s}: {results[i,0]:.3E} (+{results[i,1]:.3E} -{results[i,2]:.3E})")
+    return flat_samples
 
-    return results
+"""
+iterating function for hist_error
+"""
+# the posterior function is just ( (N+1) * P(X=k), X~Binom(N,f)) )
+def post(prob, k) :
+    posterior = (N+1) * binom(N, prob).pmf(k)
+    return posterior
+
+# find x such that \int_0^x func(x') dx' = target
+def find_cum_at(func, k, target) :
+    cumulative = lambda x: quad(lambda prob: func(prob, k), 0, x)[0] - target
+    try :
+        result = bisect(cumulative, 0.0, 1.0)
+    except ValueError :
+        sys.exit(f"k={k}, f(a)={cumulative(0.0)}, f(b)={cumulative(1.0)}")
+    return result
+
+def find_error(hist_item) :
+    median = find_cum_at(post, hist_item, 0.50)
+    error_16 = np.abs(median-find_cum_at(post, hist_item, 0.16))
+    error_84 = np.abs(median-find_cum_at(post, hist_item, 0.84))
+
+    return (median, error_16, error_84)
+
 
 def hist_errors(hist, poisson=False) :
     """
@@ -166,44 +183,18 @@ def hist_errors(hist, poisson=False) :
 
     # use Bayes theorem instead
     else :
-        # the posterior function is just ( (N+1) * P(X=k), X~Binom(N,f)) )
-        def post(f) :
-            posterior = (N+1) * binom(N, f).pmf(k)
-            return posterior
+        def init_pool(hist) :
+            global N
+            N = np.sum(hist)
+        with multiprocessing.Pool(None, init_pool(hist)) as pool :
+            mapped = pool.map(find_error, hist)
 
-        # find x such that \int_0^x func(x') dx' = target
-        def find_cum_at(func, target) :
-            cumulative = lambda x: quad(func, 0, x)[0] - target
-            try :
-                result = bisect(cumulative, 0.0, 1.0)
-            except ValueError :
-                sys.exit(f"k={k}, f(a)={cumulative(0.0)}, f(b)={cumulative(1.0)}")
-            return result
+        mapped = np.array(mapped)
+        medians = mapped[:, 0] * N
+        errors[0] = mapped[:, 1] * N
+        errors[1] = mapped[:, 2] * N
 
-        # iterate over each bin
-        for i in range(len(hist)) :
-            print(f"calculating errors: {i/len(hist)*100:.2f}%", end='\r')
-            k = hist[i]
-
-            # only perform Bayesian error for small bins
-            if k < np.sqrt(N)/2 :
-                # find the 16th percentile for the posterior function
-                f_16 = find_cum_at(post, 0.16)
-
-                # find the 84th percentile for the posterior function
-                f_84 = find_cum_at(post, 0.84)
-
-                medians[i] = N*find_cum_at(post, 0.50)
-                errors[0, i] = np.abs(medians[i]-N*find_cum_at(post, 0.16))
-                errors[1, i] = np.abs(medians[i]-N*find_cum_at(post, 0.84))
-
-            # for the big bins, calculate Poisson error
-            else :
-                p = hist[i]/N
-                error_poisson = np.sqrt(p*(1-p)/N)*N
-
-                medians[i] = hist[i]
-                errors[:, i] = [error_poisson]*2
+        return medians, errors
 
     print("error calculation complete!")
     return medians, errors
